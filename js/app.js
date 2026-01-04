ef458dabfef4bb07d543f8b1d56e87dbbad23069
Antreas_Drak <scralen01@hotmail.com>
2026-01-04 16:15:37 +0100

    Trying to make the tabs work

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
 const buttons = document.querySelectorAll('.tab-button');
 const categories = document.querySelectorAll('.product-category');
 const showMoreBtn = document.getElementById('show-more');
+
+// Main tabs - sub tabs
 const mainWindowBtn = document.querySelectorAll('.window-button');
+const subTabsBtn = document.querySelectorAll('.windows-content');
+
+
+mainWindowBtn.forEach(mWin => {
+    mWin.addEventListener('click', () => {
+
+        // Deactivating all main tabs
+        mainWindowBtn.forEach(btn => btn.classList.remove(active));
+
+        // Hidding all the main content
+        subTabsBtn.forEach(section => section.classList.remove('active'));
+
+        // Activating clicked tab
+        mWin.classList.add('active');
+
+        // Showing linked content
+        const contentId = 
+    })
+})
